5327bf26b7f22727a3f26c34bbe666eda99944e9
bambalic <lakiwow95@gmail.com>
2021-05-04 19:20:23 +0200

    Added tshirt schema
    Added tshirt schema in api/models.

---
+++ backend/api/models/tshirt.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const tshirtSchema=mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    tshirtName:{
+        type:String,
+        required:true
+    },
+    stock:[{
+        size:{
+            // ["S","M","L","XL"]
+            type:String,
+            required:true,
+            enum:["S","M","L","XL"]
+        },
+        quantity:{
+            type:Number,
+            default:0
+        },
+
+    }],
+    // image:{
+    //     type:String,
+    //     // default:""
+    // },
+    price:{
+        type:Number,
+        required:true
+    }
+
+});
+
+module.exports=mongoose.model('Tshirt',tshirtSchema);
