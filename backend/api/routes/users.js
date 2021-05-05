593bde5c8cb0cc7b2b273f326efcbf676b930d08
bambalic <lakiwow95@gmail.com>
2021-05-05 05:27:53 +0200

    Updated requests handling on users routes
    Added handling for patch request on users route. Patch request for changing password.

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -6,7 +6,7 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 router.post("/signup", (req, res, next) => {
-  User.findOne({email: req.body.email})
+  User.findOne({ email: req.body.email })
     .exec()
     .then((user) => {
       if (user) {
@@ -35,7 +35,7 @@ router.post("/signup", (req, res, next) => {
                     email: req.body.email,
                     username: req.body.username,
                     password: hash,
-                    level: req.body.level
+                    level: req.body.level,
                   });
                   user
                     .save()
@@ -60,71 +60,71 @@ router.post("/signup", (req, res, next) => {
 });
 
 router.post("/login", (req, res, next) => {
-  User.findOne({email: req.body.email})
-  .exec()
-  .then(user=>{
-      if(!user){
-          return res.status(401).json({
-              message: "Login failed"
-          });
-      }else{
-          bcrypt.compare(req.body.password,user.password,(error,result)=>{
-            // comparison failed  
-            if(error){
-                return res.status(401).json({
-                    message: "Login failed"
-                });
-              }
-              // correct password
-              if (result){
-                  return res.status(200).json({
-                      message: "Login successful"
-                  })
-              // incorrect password
-              }else{
-                return res.status(401).json({
-                    message: "Login failed"
-                });
-              }
-          });
-      }
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json({
-      error: err,
-    });
-  });
-});
-
-router.delete('/:userId',(req,res,next)=>{
-    User.deleteOne({_id:req.params.userId})
+  User.findOne({ email: req.body.email })
     .exec()
-    .then(result=>{
-        console.log(result)
-        res.status(200).json({
-            message:"Deleted successfully"
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Login failed",
         });
+      } else {
+        bcrypt.compare(req.body.password, user.password, (error, result) => {
+          // comparison failed
+          if (error) {
+            return res.status(401).json({
+              message: "Login failed",
+            });
+          }
+          // correct password
+          if (result) {
+            return res.status(200).json({
+              message: "Login successful",
+            });
+            // incorrect password
+          } else {
+            return res.status(401).json({
+              message: "Login failed",
+            });
+          }
+        });
+      }
     })
     .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: err,
-        });
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
-router.get("/",(req,res,next)=>{
-    User.find()
+router.delete("/:userId", (req, res, next) => {
+  User.deleteOne({ _id: req.params.userId })
     .exec()
-    .then(users=>{
-        const response = {
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: "Deleted successfully",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get("/", (req, res, next) => {
+  User.find()
+    .exec()
+    .then((users) => {
+      const response = {
         numberOfUsers: users.length,
         usersSignedUp: users.map((user) => {
           return {
             _id: user._id,
             email: user.email,
-            username: user.username
+            username: user.username,
           };
         }),
       };
@@ -136,9 +136,65 @@ router.get("/",(req,res,next)=>{
         error: err,
       });
     });
+});
 
-    
-})
-
+// we get email currentPassword and newPassword
+router.patch("/", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      // exists user with given email
+      if (user) {
+        bcrypt.compare(
+          req.body.currentPassword,
+          user.password,
+          (error, result) => {
+            // comparison failed
+            if (error) {
+              return res.status(401).json({
+                message: "Failed",
+              });
+            }
+            // correct password
+            if (result) {
+              bcrypt.hash(req.body.newPassword, 10, (err, hash) => {
+                if (err) {
+                  return res.status(500).json({
+                    error: err,
+                  });
+                } else {
+                  user.password = hash;
+                  user
+                    .save()
+                    .then((result) => {
+                      return res.status(200).json({
+                        message: "Successfully changed password",
+                      });
+                    })
+                    .catch((err) => {
+                      console.log(err);
+                      res.status(500).json({
+                        error: err,
+                      });
+                    });
+                }
+              });
+              // incorrect password
+            } else {
+              return res.status(401).json({
+                message: "Incorrect password",
+              });
+            }
+          }
+        );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 
 module.exports = router;
