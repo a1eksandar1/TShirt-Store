cfcd26ae5e19980adfe1f4c62ad75cd2c8ae6afb
bambalic <lakiwow95@gmail.com>
2021-05-05 07:12:28 +0200

    Added controller for tshirts
    Refactored tshirts route file and moved request handling to controller.

---
+++ backend/api/controllers/tshirts.js
@@ -0,0 +1,205 @@
+const mongoose = require("mongoose");
+const path = require("path");
+
+
+const Tshirt = require("../models/tshirt");
+
+module.exports.tshirtsGetAll=(req, res, next) => {
+    Tshirt.find()
+      .exec()
+      .then((tshirts) => {
+        const response = {
+          numberOfTshirts: tshirts.length,
+          tshirtsAvailable: tshirts.map((tshirt) => {
+            // removing _id from stock (for response)
+            let stock = [];
+            for (entry of tshirt.stock) {
+              //   console.log(entry);
+              stock.push({
+                size: entry.size,
+                quantity: entry.quantity,
+              });
+            }
+            return {
+              _id: tshirt._id,
+              name: tshirt.tshirtName,
+              price: tshirt.price,
+              stock: stock,
+              image: tshirt.image,
+            };
+          }),
+        };
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
+
+module.exports.tshirtsGetById=(req, res, next) => {
+    const id = req.params.tshirtId;
+    Tshirt.findById(id)
+      .exec()
+      .then((tshirt) => {
+        console.log(tshirt);
+  
+        if (tshirt) {
+          // removing _id from stock (for response)
+          let stock = [];
+          for (entry of tshirt.stock) {
+            console.log(entry);
+            stock.push({
+              size: entry.size,
+              quantity: entry.quantity,
+            });
+          }
+  
+          res.status(200).json({
+            tshirt: {
+              _id: tshirt._id,
+              tshirtName: tshirt.tshirtName,
+              stock: stock,
+              image: tshirt.image,
+            },
+          });
+        } else {
+          res.status(404).json({
+            message: "No tshirt with given id",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  }
+
+module.exports.tshirtsPost=(req, res, next) => {
+    Tshirt.findOne({ tshirtName: req.body.tshirtName })
+      .exec()
+      .then((tshirt) => {
+        if (tshirt) {
+          return res.status(409).json({
+            message:
+              "Tshirt with that name exists, change name or use patch request",
+          });
+        } else {
+          const tshirt = new Tshirt({
+            _id: new mongoose.Types.ObjectId(),
+            tshirtName: req.body.tshirtName,
+            stock: req.body.stock,
+            price: req.body.price,
+            // if we dont send image use default
+            image:
+              req.file === undefined
+                ? "../../assets/images/tshirtDefault.png"
+                : req.file.path.split(path.sep).join(path.posix.sep),
+          });
+  
+          tshirt
+            .save()
+            .then((result) => {
+              console.log(result);
+  
+              // removing _id from stock (for response)
+              let stock = [];
+              for (entry of result.stock) {
+                console.log(entry);
+                stock.push({
+                  size: entry.size,
+                  quantity: entry.quantity,
+                });
+              }
+  
+              res.status(201).json({
+                message: "Added tshirt",
+                addedTshirt: {
+                  _id: result._id,
+                  name: result.name,
+                  stock: stock,
+                  price: result.price,
+                  image: result.image,
+                },
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+              res.status(500).json({
+                error: err,
+              });
+            });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
+
+module.exports.tshirtsPatchById=(req, res, next) => {
+    const id = req.params.tshirtId;
+    const updateOptions = {};
+    for (const option of Object.keys(req.body)) {
+      updateOptions[option] = req.body[option];
+    }
+  
+    // console.log(updateOptions);
+  
+    // runValidators=true for checking enum
+    Tshirt.updateOne(
+      { _id: id },
+      { $set: updateOptions },
+      { runValidators: true }
+    )
+      .exec()
+      .then((result) => {
+        if (result.nModified == 0) {
+          res.status(404).json({
+            message: "No tshirt with given id",
+          });
+        } else {
+          // console.log(result);
+          res.status(200).json({
+            message: "Tshirt updated",
+            check: {
+              type: "GET",
+              url: "http://localhost:3000/tshirts/" + id,
+            },
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
+
+module.exports.tshirtsDeleteById=(req, res, next) => {
+    const id = req.params.tshirtId;
+    Tshirt.deleteOne({ _id: id })
+      .exec()
+      .then((result) => {
+        if (result.deletedCount === 0) {
+          res.status(404).json({
+            message: "Tshirt with given id not available",
+          });
+        } else {
+          res.status(200).json({
+            message: "Tshirt deleted",
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: err,
+        });
+      });
+  }
--- backend/api/routes/tshirts.js
+++ backend/api/routes/tshirts.js
@@ -1,224 +1,35 @@
 const express = require("express");
 const router = express.Router();
 
-const mongoose = require("mongoose");
 const multer = require("multer");
-const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "./assets/images");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
 
 const checkAuth=require("../middleware/checkAuth");
 const checkAdminAuth=require("../middleware/checkAdminAuth");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./assets/images");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+const tshirtsController=require("../controllers/tshirts")
 
-const upload = multer({ storage: storage });
 
-const Tshirt = require("../models/tshirt");
 
-router.get("/", (req, res, next) => {
-  Tshirt.find()
-    .exec()
-    .then((tshirts) => {
-      const response = {
-        numberOfTshirts: tshirts.length,
-        tshirtsAvailable: tshirts.map((tshirt) => {
-          // removing _id from stock (for response)
-          let stock = [];
-          for (entry of tshirt.stock) {
-            //   console.log(entry);
-            stock.push({
-              size: entry.size,
-              quantity: entry.quantity,
-            });
-          }
-          return {
-            _id: tshirt._id,
-            name: tshirt.tshirtName,
-            price: tshirt.price,
-            stock: stock,
-            image: tshirt.image,
-          };
-        }),
-      };
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.get("/", tshirtsController.tshirtsGetAll);
 
-router.get("/:tshirtId", (req, res, next) => {
-  const id = req.params.tshirtId;
-  Tshirt.findById(id)
-    .exec()
-    .then((tshirt) => {
-      console.log(tshirt);
+router.get("/:tshirtId", tshirtsController.tshirtsGetById);
 
-      if (tshirt) {
-        // removing _id from stock (for response)
-        let stock = [];
-        for (entry of tshirt.stock) {
-          console.log(entry);
-          stock.push({
-            size: entry.size,
-            quantity: entry.quantity,
-          });
-        }
+router.post("/", checkAuth,checkAdminAuth, upload.single("image"), tshirtsController.tshirtsPost);
 
-        res.status(200).json({
-          tshirt: {
-            _id: tshirt._id,
-            tshirtName: tshirt.tshirtName,
-            stock: stock,
-            image: tshirt.image,
-          },
-        });
-      } else {
-        res.status(404).json({
-          message: "No tshirt with given id",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({ error: err });
-    });
-});
+router.patch("/:tshirtId",checkAuth,checkAdminAuth, tshirtsController.tshirtsPatchById);
 
-router.post("/", checkAuth,checkAdminAuth, upload.single("image"), (req, res, next) => {
-  Tshirt.findOne({ tshirtName: req.body.tshirtName })
-    .exec()
-    .then((tshirt) => {
-      if (tshirt) {
-        return res.status(409).json({
-          message:
-            "Tshirt with that name exists, change name or use patch request",
-        });
-      } else {
-        const tshirt = new Tshirt({
-          _id: new mongoose.Types.ObjectId(),
-          tshirtName: req.body.tshirtName,
-          stock: req.body.stock,
-          price: req.body.price,
-          // if we dont send image use default
-          image:
-            req.file === undefined
-              ? "../../assets/images/tshirtDefault.png"
-              : req.file.path.split(path.sep).join(path.posix.sep),
-        });
-
-        tshirt
-          .save()
-          .then((result) => {
-            console.log(result);
-
-            // removing _id from stock (for response)
-            let stock = [];
-            for (entry of result.stock) {
-              console.log(entry);
-              stock.push({
-                size: entry.size,
-                quantity: entry.quantity,
-              });
-            }
-
-            res.status(201).json({
-              message: "Added tshirt",
-              addedTshirt: {
-                _id: result._id,
-                name: result.name,
-                stock: stock,
-                price: result.price,
-                image: result.image,
-              },
-            });
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-              error: err,
-            });
-          });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.patch("/:tshirtId",checkAuth,checkAdminAuth, (req, res, next) => {
-  const id = req.params.tshirtId;
-  const updateOptions = {};
-  for (const option of Object.keys(req.body)) {
-    updateOptions[option] = req.body[option];
-  }
-
-  // console.log(updateOptions);
-
-  // runValidators=true for checking enum
-  Tshirt.updateOne(
-    { _id: id },
-    { $set: updateOptions },
-    { runValidators: true }
-  )
-    .exec()
-    .then((result) => {
-      if (result.nModified == 0) {
-        res.status(404).json({
-          message: "No tshirt with given id",
-        });
-      } else {
-        // console.log(result);
-        res.status(200).json({
-          message: "Tshirt updated",
-          check: {
-            type: "GET",
-            url: "http://localhost:3000/tshirts/" + id,
-          },
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
-
-router.delete("/:tshirtId",checkAuth,checkAdminAuth, (req, res, next) => {
-  const id = req.params.tshirtId;
-  Tshirt.deleteOne({ _id: id })
-    .exec()
-    .then((result) => {
-      if (result.deletedCount === 0) {
-        res.status(404).json({
-          message: "Tshirt with given id not available",
-        });
-      } else {
-        res.status(200).json({
-          message: "Tshirt deleted",
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.delete("/:tshirtId",checkAuth,checkAdminAuth, tshirtsController.tshirtsDeleteById);
 
 module.exports = router;
