58e69fea15fc5e4e4c73e4406cbdf9ded5831804
m97s <matijasreckovic97@gmail.com>
2021-05-14 02:26:54 +0200

    Initial shitty layout

--- frontend/src/app/pages/product/product.component.ts
+++ frontend/src/app/pages/product/product.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  // ovo treba dodati u model
+  // msm i ne moramo
+  // necu onda jos da dodajem rating sistem
+  // rating = 4.5;
+  // numberOfRatings = 20;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // changeRating(newRating : number){
+  //   this.rating = newRating;
+  // }
 
 }
